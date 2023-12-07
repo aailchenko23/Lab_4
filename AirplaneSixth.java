41494a2a4e93f679b713fe2bcf5ff1d29fd144e3
aailchenko23 <125138023+aailchenko23@users.noreply.github.com>
2023-12-07 09:08:03 +0200

    Add files via upload

---
+++ AirplaneSixth.java
@@ -0,0 +1,29 @@
+package Airplans;
+
+public class AirplaneSixth extends AirplaneThree {
+    // Метод класу AirplaneSixth, який викликає метод з зовнішнього класу AirplaneThree
+    public AirplaneSixth(double speed, int passenger, String model) {
+        super(speed, passenger, model);
+    }
+    void methodSixth() {
+        getModel();  // Виклик методу з класу AirplaneThree
+        System.out.println("method sixth does something");
+
+    }
+
+    // Метод для тестування внутрішнього класу AirplaneSixthDemo
+    void test() {
+        AirplaneSixthDemo demo = new AirplaneSixthDemo();  // Створення об'єкта внутрішнього класу
+        demo.methodSixthDemo();  // Виклик методу внутрішнього класу
+    }
+
+    // Внутрішній клас AirplaneSixthDemo
+    class AirplaneSixthDemo {
+        // Метод внутрішнього класу, який виводить повідомлення та викликає метод зовнішнього класу
+        void methodSixthDemo() {
+            System.out.println("method sixthDemo does something");
+            methodSixth();  // Виклик методу зовнішнього класу AirplaneSixth
+        }
+    }
+
+}
